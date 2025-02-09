bc5fe4c68847cbaf1620da8bf200b56f2e77686f
Leandro Coutinho <lbcoutinho.blm@gmail.com>
2025-02-09 21:51:45 +0000

    Setup flyway and create DB init SQL

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("org.postgresql:postgresql")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	testRuntimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
---
+++ src/main/resources/db/migration/V1__init_db.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS "authors";
+CREATE TABLE "authors" (
+   "id" serial PRIMARY KEY,
+   "age" smallint,
+   "description" VARCHAR(512),
+   "image" VARCHAR(512),
+   "name" VARCHAR(512)
+);
+
+DROP TABLE IF EXISTS "books";
+CREATE TABLE "books" (
+   "isbn" VARCHAR(19) PRIMARY KEY,
+   "description" VARCHAR(2048),
+   "image" VARCHAR(512),
+   "title" VARCHAR(512),
+   "author_id" bigint
+);
